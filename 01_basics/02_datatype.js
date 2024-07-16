5171622d46cfc220d606cfe94d9312491ac2110f
AlokSrivastavaa <131744578+AlokSrivastavaa@users.noreply.github.com>
2024-07-16 08:43:21 +0000

    discuss on variables

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser 
+
+// for studying documentation we use MDN and tc.39.es
+
+
+
+
+
+
+let name = "alok"
+let age = 18 
+let isLoggedIn = false 
+
+// Primitive Data type
+// number=> 2 to power 53
+// bigint
+// string =>""
+// boolean =>true/false 
+// null => standalone value
+// undefined =>
+// symbol
+
+
+// object 
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object 
